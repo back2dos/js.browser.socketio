6d5bc87c45f81933a048b163bd9fbac05a2b402c
LUXOFFICE\balazs.zubak <balazs.zubak@doclerholding.com>
2017-11-10 16:15:37 +0100

    added SocketEvent abstract to be typesafe

--- src/js/browser/SocketIo.hx
+++ src/js/browser/SocketIo.hx
@@ -62,3 +62,96 @@ typedef ManagerOpts = {
   ?timeout: Int,
   ?transports: Array<String>
 }
+
+@:enum abstract SocketEvent<T>(String) {
+
+  /**
+     * Fired upon a connection including a successful reconnection.
+     * socket.on('connect', () => {
+     *   // ...
+     * });
+     *
+     * // note: you should register event handlers outside of connect,
+     * // so they are not registered again on reconnection
+     * socket.on('myevent', () => {
+     *   // ...
+     * });
+     */
+  var Connect:SocketEvent<Void->Void> = "connect";
+
+  /**
+    * Fired upon a connection error.
+    *
+    * @param error (Object) error object
+    */
+  var ConnectError:SocketEvent<SocketError->Void> = "connect_error";
+
+  /**
+    * Fired upon a connection timeout.
+    *
+    * @param timeout (Object) timeout???
+    */
+  var ConnectTimeout:SocketEvent<Dynamic->Void> = "connect_timeout";
+
+  /**
+    * Fired when an error occurs.
+    *
+    * @param error (Object) error object
+    */
+  var Error:SocketEvent<SocketError->Void> = "error";
+
+  /**
+    * Fired upon a disconnection.
+    *
+    * @param reason (String) either 'io server disconnect' or 'io client disconnect'
+    */
+  var Disconnect:SocketEvent<String->Void> = "disconnect";
+
+  /**
+    * Fired upon a successful reconnection.
+    *
+    * @param attempt (Number) reconnection attempt number
+    */
+  var Reconnect:SocketEvent<UInt->Void> = "reconnect";
+
+  /**
+    * Fred upon an attempt to reconnect.
+    *
+    * @param attempt (Number) reconnection attempt number
+    */
+  var ReconnectAttempt:SocketEvent<UInt->Void> = "reconnect_attempt";
+
+  /**
+    * Fired upon an attempt to reconnect..
+    *
+    * @param attempt (Number) reconnection attempt number
+    */
+  var Reconnecting:SocketEvent<UInt->Void> = "reconnecting";
+
+  /**
+    * Fired upon a reconnection attempt error.
+    *
+    * @param error (Object) error object
+    */
+  var ReconnectError:SocketEvent<SocketError->Void> = "reconnect_error";
+
+  /**
+    * Fired when couldn't reconnect within reconnectionAttempts.
+    */
+  var ReconnectFailed:SocketEvent<Void->Void> = "reconnect_failed";
+
+  /**
+    * Fired when a ping packet is written out to the server.
+    */
+  var Ping:SocketEvent<Void->Void> = "ping";
+
+  /**
+    * Fired when couldn't reconnect within reconnectionAttempts.
+    *
+    * @param latency (Number) number of ms elapsed since ping packet (i.e.: latency).
+    */
+  var Pong:SocketEvent<UInt->Void> = "pong";
+
+}
+
+typedef SocketError = Dynamic;
